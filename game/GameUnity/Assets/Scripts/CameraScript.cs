c6f618560834e6929050f185d9236f5cd6ee4ba0
Nikita Shijan <nikusia0804@gmail.com>
2020-11-30 19:33:32 +1000

    feature/camera script

---
+++ game/GameUnity/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+  public GameObject astro;
+
+  void Update()
+  {
+    if (astro.transform.position.x > transform.position.x + 10)
+    {
+      transform.position = new Vector3(astro.transform.position.x + 10, transform.position.y, transform.position.z);
+    }
+    if (astro.transform.position.x < transform.position.x - 10)
+    {
+      transform.position = new Vector3(astro.transform.position.x - 10, transform.position.y, transform.position.z);
+    }
+  }
+}
