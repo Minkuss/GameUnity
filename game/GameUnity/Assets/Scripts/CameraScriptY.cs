4779178e3b3870b96d704230a836a82a749341af
Nikita Shijan <nikusia0804@gmail.com>
2020-12-04 22:15:55 +1000

    mainLVL

---
+++ game/GameUnity/Assets/Scripts/CameraScriptY.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScriptY : MonoBehaviour
+{
+  public GameObject astro;
+
+  void Update()
+  {
+    if (astro.transform.position.y > transform.position.y)
+    {
+      transform.position = new Vector3(transform.position.x, transform.position.y  +  5, transform.position.z);
+    }
+    if (astro.transform.position.y < transform.position.y)
+    {
+      transform.position = new Vector3(transform.position.x, transform.position.y - 5, transform.position.z);
+    }
+  }
+}
