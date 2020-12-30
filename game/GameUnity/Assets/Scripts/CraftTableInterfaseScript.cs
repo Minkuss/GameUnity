423cb3208e10a3d9dac2fe63135f6010a52e4359
Nikita Shijan <nikusia0804@gmail.com>
2020-12-31 00:39:17 +1000

    СКРИПППППТПТПТПТ
    Добавил скрипт появления интерфейса верстака

---
+++ game/GameUnity/Assets/Scripts/CraftTableInterfaseScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftTableInterfaseScript : MonoBehaviour
+{
+  void Start()
+  {
+    gameObject.SetActive(false);
+  }
+
+  public void visibility()
+  {
+    if (gameObject.activeSelf == false)
+    {
+      gameObject.SetActive(true);
+    }
+    else
+    {
+      gameObject.SetActive(false);
+    }
+  }
+}
